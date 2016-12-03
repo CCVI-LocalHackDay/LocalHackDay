0d79e4c581fe0b050f1604189266f96926c38cca
Fred Chun <hyunginfredchun@gmail.com>
2016-12-03 09:53:05 -0500

    Commit Message

--- frontend.py
+++ frontend.py
@@ -1,8 +1,6 @@
-from tkinter import*
+from tkinter import *
 from matplotlib import *
 
-
-
 root = Tk()
-mainFrame = Frame(root,height=1000, width=800)
+mainFrame = Frame(root, height=1000, width=800)
 mainFrame.grid()
