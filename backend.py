809c2b881096f1466fcb62d1f7985217cd4cc078
Advait Maybhate <advait.maybhate9@gmail.com>
2016-12-03 10:01:35 -0500

    test twitter with this code

--- backend.py
+++ backend.py
@@ -1,4 +1,13 @@
 from twitter import *
 import config
-a = config.twitterKeys["consumerKey"]
-print(a)
+twitter = Twitter(auth=OAuth(config.twitterKeys["accessKey"], config.twitterKeys["accessSecret"], config.twitterKeys["consumerKey"], config.twitterKeys["consumerSecret"]))
+#query = twitter.search.tweets(q="music", geocode = "", count=100, max_id=None, until="2016-10-22")
+query = twitter.search.tweets(q = "lazy dog", count = 100)
+for result in query["statuses"]:
+        # -----------------------------------------------------------------------
+        # only process a result if it has a geolocation
+        # -----------------------------------------------------------------------
+        user = result["user"]["screen_name"]
+        text = result["text"]
+        text = text.encode('ascii', 'replace')
+        print(text)
