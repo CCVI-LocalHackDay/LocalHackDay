b504b9eefff70b13e507cfceec2ae987961fcecc
coolcom200 <leonfattakhov@yahoo.com>
2016-12-03 12:14:40 -0500

    Multi-embedded matplotlib graphs

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ from twitter import *
 import config
 twitter = Twitter(auth=OAuth(config.twitterKeys["accessKey"], config.twitterKeys["accessSecret"], config.twitterKeys["consumerKey"], config.twitterKeys["consumerSecret"]))
 #query = twitter.search.tweets(q="music", geocode = "", count=100, max_id=None, until="2016-10-22")
-query = twitter.search.tweets(q = "lazy dog", count = 100)
+query = twitter.search.tweets(q = "dog", count = 100)
 for result in query["statuses"]:
         user = result["user"]["screen_name"]
         text = result["text"]
